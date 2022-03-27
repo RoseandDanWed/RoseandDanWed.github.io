ff31ea508d1f7bb18d209e1d6f428636733081ef
JohnandAshWed <97843668+JohnandAshWed@users.noreply.github.com>
2022-03-27 14:40:11 +0100

    Changing calendar details

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "John and Ashleys's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Apr 30, 2023 12:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('May 1, 2023 01:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'The Vu, Ballencrieff Reservoir, Bathgate, West Lothian, EH48 4LD, UK',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day. For any queries or issues, please contact Johnandashwed@gmail.com"
         }
     });
 
